5e149922b37dfaecee200a8a388c933f377d668d
justsamuel <samuel.oosterholt20001004@gmail.com>
2021-11-09 12:07:14 +0100

    Code cleanup

--- api.py
+++ api.py
@@ -1,11 +1,17 @@
-import random, string, sys
+import random
+import string
+import sys
+
 import requests
+
+s = requests.session()
+
 
 def __post_request(url, json_data):
     api_url = f"{api_host}/{url}"
     headers = {'X-API-Key': api_key, 'Content-type': 'application/json'}
 
-    req = requests.post(api_url, headers=headers, json=json_data)
+    req = s.post(api_url, headers=headers, json=json_data)
     rsp = req.json()
     req.close()
 
@@ -14,24 +20,25 @@ def __post_request(url, json_data):
 
     if not "type" in rsp or not "msg" in rsp:
         sys.exit(f"API {url}: got response without type or msg from Mailcow API")
-    
+
     if rsp['type'] != 'success':
         sys.exit(f"API {url}: {rsp['type']} - {rsp['msg']}")
+
 
 def add_user(email, name, active, quotum):
     password = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
     json_data = {
-        'local_part':email.split('@')[0],
-        'domain':email.split('@')[1],
-        'name':name,
-        'quota':str(quotum),
-        'password':password,
-        'password2':password,
+        'local_part': email.split('@')[0],
+        'domain': email.split('@')[1],
+        'name': name,
+        'quota': str(quotum),
+        'password': password,
+        'password2': password,
         "active": 1 if active else 0
     }
 
     __post_request('api/v1/add/mailbox', json_data)
-    
+
     json_data = {
         'items': [email],
         'attr': {
@@ -41,25 +48,26 @@ def add_user(email, name, active, quotum):
                 "spam_score",
                 "spam_policy",
                 "delimiter_action",
-                #"syncjobs",
-                #"eas_reset",
+                # "syncjobs",
+                # "eas_reset",
                 "quarantine",
-                #"sogo_profile_reset",
-                #"quarantine_attachments",
+                # "sogo_profile_reset",
+                # "quarantine_attachments",
                 "quarantine_notification",
-                #"app_passwds",
+                # "app_passwds",
                 "pushover"
             ]
         }
     }
-    
+
     __post_request('api/v1/edit/user-acl', json_data)
+
 
 def edit_user(email, active=None, name=None):
     attr = {}
-    if (active is not None):
+    if active is not None:
         attr['active'] = 1 if active else 0
-    if (name is not None):
+    if name is not None:
         attr['name'] = name
 
     json_data = {
@@ -69,25 +77,27 @@ def edit_user(email, active=None, name=None):
 
     __post_request('api/v1/edit/mailbox', json_data)
 
+
 def __delete_user(email):
     json_data = [email]
 
     __post_request('api/v1/delete/mailbox', json_data)
 
+
 def check_user(email):
     url = f"{api_host}/api/v1/get/mailbox/{email}"
     headers = {'X-API-Key': api_key, 'Content-type': 'application/json'}
-    req = requests.get(url, headers=headers)
+    req = s.get(url, headers=headers)
     rsp = req.json()
     req.close()
-    
+
     if not isinstance(rsp, dict):
         sys.exit("API get/mailbox: got response of a wrong type")
 
-    if (not rsp):
-        return (False, False, None)
+    if not rsp:
+        return False, False, None
 
     if 'active_int' not in rsp and rsp['type'] == 'error':
         sys.exit(f"API {url}: {rsp['type']} - {rsp['msg']}")
-    
-    return (True, bool(rsp['active_int']), rsp['name'])
+
+    return True, bool(rsp['active_int']), rsp['name']
