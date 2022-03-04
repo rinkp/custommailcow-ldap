08e775552c1b466a5693bc687d7432489aefd447
Gijs de Man <g.d.man2@student.tue.nl>
2022-03-04 19:15:20 +0100

    Partially transfer syncer.py to syncer.ts, partially adjust Dockerfile, partially wondering what the fuck I am doing

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,63 @@
-FROM python:3-alpine3.14
+# FROM python:3-alpine3.14
 
-RUN apk --no-cache add build-base openldap-dev python2-dev python3-dev
-RUN pip3 install --upgrade pip
-RUN pip3 install python-ldap sqlalchemy requests
+# RUN apk --no-cache add build-base openldap-dev python2-dev python3-dev
+# RUN pip3 install --upgrade pip
+# RUN pip3 install python-ldap sqlalchemy requests
 
 COPY templates ./templates
-COPY api.py filedb.py syncer.py ./
+# Wat hiermee?
+# COPY api.py filedb.py syncer.py ./
 
 VOLUME [ "/db" ]
 VOLUME [ "/conf/dovecot" ]
 VOLUME [ "/conf/sogo" ]
 
-ENTRYPOINT [ "python3", "syncer.py" ]
+# ENTRYPOINT [ "python3", "syncer.py" ]
+
+# We build our container using node:13-alpine
+FROM node:13-alpine AS builder
+ENV NODE_ENV=development
+
+# Change dir to install dir.
+WORKDIR /usr/src/custommailcow-ldap
+
+# Copy over the package and package-lock
+COPY package*.json .
+
+# Install dependencies
+RUN npm i
+
+# Copy over the tsconfig
+COPY tsconfig.json .
+
+# Copy over the source files
+COPY src /usr/src/custommailcow-ldap/src
+
+# Transpile the typescript files
+RUN npm run build
+
+
+# Create production container.
+FROM node:13-alpine
+
+# Set correct dir.
+WORKDIR /usr/src/custommailcow-ldap
+
+# Copy over the package and package-lock
+COPY package*.json .
+
+# Install production dependencies
+RUN npm install --only=production
+
+# Copy over the user data
+COPY data /usr/src/custommailcow-ldap/data
+
+# Copy over the source files from the builder
+COPY --from=builder /usr/src/custommailcow-ldap/dist ./src
+
+# Set correct priv.
+RUN chown -R node:node .
+USER node
+
+EXPOSE 9194
+CMD ["node", "src/index.js"]
---
+++ src/intex.ts
@@ -0,0 +1,65 @@
+﻿import { Client } from 'ldapts';
+
+// type config
+
+let config : any;
+
+function initialization() {
+    read_config()
+}
+
+initialization()
+
+async function sync() {
+    let ldap_connector = new Client({
+        url: config['LDAP_URI'],
+    })
+    await ldap_connector.bind(config['LDAP_BIND_DN'], config['LDAP_BIND_DN_PASSWORD'])
+
+    let ldap_results = ldap_connector.search(config['LDAP_BASE_DN'],{
+        scope: 'sub',
+        filter: config['LDAP_FILTER']
+        // TODO -> wat hiermee doen?
+        // ['mail', 'displayName', 'userAccountControl']
+    })
+
+    // TODO -> session time in filedb zetten?
+
+    console.log(ldap_results)
+}
+
+function read_config() {
+    let required_config_keys = [
+        'LDAP-MAILCOW_LDAP_URI',
+        'LDAP-MAILCOW_LDAP_GC_URI',
+        'LDAP-MAILCOW_LDAP_DOMAIN',
+        'LDAP-MAILCOW_LDAP_BASE_DN',
+        'LDAP-MAILCOW_LDAP_BIND_DN',
+        'LDAP-MAILCOW_LDAP_BIND_DN_PASSWORD',
+        'LDAP-MAILCOW_API_HOST',
+        'LDAP-MAILCOW_API_KEY',
+        'LDAP-MAILCOW_SYNC_INTERVAL'
+    ]
+
+    for (let config_key in required_config_keys) {
+        if (!(config_key in process.env)) throw new Error(`Required environment value ${config_key} is not set`)
+    }
+
+    if ('LDAP-MAILCOW_LDAP_FILTER' in process.env && !('LDAP-MAILCOW_SOGO_LDAP_FILTER' in process.env))
+        throw new Error('LDAP-MAILCOW_SOGO_LDAP_FILTER is required when you specify LDAP-MAILCOW_LDAP_FILTER')
+
+    if ('LDAP-MAILCOW_SOGO_LDAP_FILTER' in process.env && !('LDAP-MAILCOW_LDAP_FILTER' in process.env))
+        throw new Error('LDAP-MAILCOW_LDAP_FILTER is required when you specify LDAP-MAILCOW_SOGO_LDAP_FILTER')
+
+    if ('LDAP-MAILCOW_LDAP_FILTER' in process.env) {
+        config['LDAP_FILTER'] = process.env['LDAP-MAILCOW_LDAP_FILTER']
+    } else {
+        config['LDAP_FILTER'] = '(&(objectClass=user)(objectCategory=person))'
+    }
+
+    if ('LDAP-MAILCOW_SOGO_LDAP_FILTER' in process.env) {
+        config['SOGO_LDAP_FILTER'] = process.env['LDAP-MAILCOW_SOGO_LDAP_FILTER']
+    } else {
+        config['SOGO_LDAP_FILTER'] = "objectClass='user' AND objectCategory='person'"
+    }
+}
